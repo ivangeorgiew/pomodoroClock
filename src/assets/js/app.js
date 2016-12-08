bbfaacb16ec50ceba5aada79689f7cd466eecb6f
Ivan Georgiev <ivan_georgiew@yahoo.com>
2016-12-08 19:43:58 +0200

    added beep sound

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,26 @@
-const al = (a) =>{
-  return 2*a;
+const minOrPlus = (op, n) => {
+  if(n === 0)
+    var time = '.span--break';
+  else
+    var time = '.span--session';
+
+  if(op === 'bigger'){
+    if(document.querySelector(time).innerHTML > 1)
+      document.querySelector(time).innerHTML -= 1; 
+  }
+  else{
+    if(document.querySelector(time).innerHTML < 60)
+      document.querySelector(time).innerHTML = +document.querySelector(time).innerHTML + 1; 
+  }
 };
-console.log(al(5));
+
+document.querySelector('.b--min').onclick = () => {minOrPlus('bigger', 0);}
+document.querySelector('.s--min').onclick = () => {minOrPlus('bigger', 1);}
+
+document.querySelector('.b--plus').onclick = () => {minOrPlus('smaller', 0);}
+document.querySelector('.s--plus').onclick = () => {minOrPlus('smaller', 1);}
+
+const audio = () {
+  new Audio('http://soundbible.com/grab.php?id=1598&type=mp3').play();
+};
 
