473c73efeab5c0192d97d5b5c150cc789427e1f0
Ivan Georgiev <ivan_georgiew@yahoo.com>
2016-12-10 21:00:31 +0200

    Finished the pomodoroClock and updated ReadMe

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-# static-boilerplate
+# pomodoroClock
 
-A boilerplate for building static websites by my own making.
+An app suitable for studying where work and break are time scheduled.
+The option to pause the clock is not added on purpose, because it would destroy
+the whole idea of productivity.
+
+View the app live at https://ivangeorgiew.github.io/wikiViewer/
+
+### Feedback
+
+**Feedback** is much **appreciated** since there are still some possibilities for a mistake.
+
+### Installation
+
+I used my [static-boilerplate](https://github.com/ivangeorgiew/static-boilerplate) repo to make the website.
+In case you want to install:
+
+1) `cd /*YOUR DIRECTORY*/`
+
+2) `git clone https://github.com/ivangeorgiew/pomodoroClock.git`
+
+3) `npm i`
+
+4) `gulp`
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,38 +1,128 @@
-const beep = new Audio('assets/beep.mp3');
-//beep.play();
+const bMin = document.querySelector('.b--min');
+const sMin = document.querySelector('.s--min');
+const bPlus = document.querySelector('.b--plus');
+const sPlus = document.querySelector('.s--plus');
+const sSes = document.querySelector('.span--session');
+const sBreak = document.querySelector('.span--break');
+const pTimer = document.querySelector('.p--timer');
+const bTimer = document.querySelector('.b--timer');
+const h2Type = document.querySelector('.h2--type');
 
-const minOrPlus = (time, op) => {
-  if(op === 'bigger'){
-    if(document.querySelector(time).innerHTML > 1){
-      document.querySelector(time).innerHTML -= 1; 
-    }
+
+
+
+//FUNCTIONS FOR SETTINGS
+//
+const minOrPlus = (time, op)=>{
+  if(op === 'min'){
+    if(time.innerHTML > 1)
+      time.innerHTML -= 1; 
   }
   else{
-    if(document.querySelector(time).innerHTML < 60){
-      document.querySelector(time).innerHTML = 
-        +document.querySelector(time).innerHTML + 1; 
-    }
+    if(time.innerHTML < 60)
+      time.innerHTML = +time.innerHTML + 1; 
   }
 };
 
-const breakOrSession = (op, n) => {
+const typeSettings = (op, n, pTimer)=>{
   if(n === 0){
-    const time = '.span--break';
+    const time = document.querySelector('.span--break');
     minOrPlus(time, op);
   }
   else{
-    const time = '.span--session';
+    const time = document.querySelector('.span--session');
     minOrPlus(time, op);
-    document.querySelector('.p--timer').innerHTML.split(':')[0] = +document.querySelector(time).innerHTML;
+    pTimer.innerHTML = 
+      time.innerHTML + ':00';
   }
+};
+//
+//--end of functions for settings
 
+
+
+
+
+//FUNCTIONS FOR TIMER
+//
+const setSecs = (t, sSes, sBreak, mins, secs, h2Type)=>{
+  if(secs > 10)
+    return mins + ':' + (secs-1).toString();
+
+  else if(secs > 0)
+    return mins + ':0' + (secs-1);
+
+  else
+    return setMins(t, sSes, sBreak, mins, h2Type);
 };
 
-document.querySelector('.b--min').onclick = () => {breakOrSession('bigger', 0);}
-document.querySelector('.s--min').onclick = () => {breakOrSession('bigger', 1);}
+const setMins = (t, sSes, sBreak, mins, h2Type)=>{
+  if(mins > 10)
+    return (mins-1).toString() + ':59';
+  
+  else if(mins > 0)
+    return (mins-1).toString() + ':59';
+  
+  else
+    return end(t, sSes, sBreak, h2Type);
+};
 
-document.querySelector('.b--plus').onclick = () => {breakOrSession('smaller', 0);}
-document.querySelector('.s--plus').onclick = () => {breakOrSession('smaller', 1);}
+const end = (t, sSes, sBreak, h2Type)=>{
+  new Audio('assets/beep.mp3').play();
+
+  if(t === 'break'){
+    h2Type.innerHTML = 'In Break';
+    return (sBreak.innerHTML-1).toString() + ':59';
+  }
+
+  else{
+    h2Type.innerHTML = 'In Session';
+    return (sSes.innerHTML-1) + ':59';
+  }
+};
+
+const run = (bTimer, h2Type)=>{
+  bMin.disabled = sMin.disabled = bPlus.disabled = sPlus.disabled = true;
+
+  const intervalID = setInterval(()=>{
+    if(h2Type.innerHTML === 'In Session'){
+      pTimer.innerHTML = 
+        setSecs('break', sSes, sBreak,
+          pTimer.innerHTML.split(':')[0],
+          pTimer.innerHTML.split(':')[1], h2Type);
+    }
+    else{
+      pTimer.innerHTML = 
+        setSecs('session', sSes, sBreak,
+          pTimer.innerHTML.split(':')[0],
+          pTimer.innerHTML.split(':')[1], h2Type);
+    }
+
+    bTimer.onclick = ()=>{
+      clearInterval(intervalID);
+      reset();
+
+      //have to set it again cuz it won't go to the bottom onclick one :D
+      bTimer.onclick = ()=>{run(bTimer, h2Type);};
+    };
+  }, 1000);
+};
+
+const reset = ()=>{
+  bMin.disabled = sMin.disabled = bPlus.disabled = sPlus.disabled = false;
+  pTimer.innerHTML = sSes.innerHTML + ':00';
+};
+
+//
+//--end of functions for timer
 
 
 
+//Settings
+bMin.onclick = ()=>{typeSettings('min', 0, pTimer);};
+sMin.onclick = ()=>{typeSettings('min', 1, pTimer);};
+bPlus.onclick = ()=>{typeSettings('plus', 0, pTimer);};
+sPlus.onclick = ()=>{typeSettings('plus', 1, pTimer);};
+
+//Timer
+bTimer.onclick = ()=>{run(bTimer, h2Type);};
